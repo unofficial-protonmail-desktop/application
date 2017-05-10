ef90cd0ff7738ba2947554f026e3490992611c89
kontrollanten <alexander@width.se>
2017-05-11 01:16:44 +0200

    Prefill account name as username at ProtonMail login page

--- src/sidebar.js
+++ src/sidebar.js
@@ -3,28 +3,48 @@ const swal = require('sweetalert');
 exports.initiateTabs = () => {
 	const TabGroup = require('electron-tabs');
 	const tabGroup = new TabGroup();
-
-	function createTab(name, active = false) {
-		const tab = tabGroup.addTab({
-			title: name,
-			src: "https://mail.protonmail.com/login?",
-			visible: true,
-			closable: false,
-			active: active
-		});
-	}
 	
-	document.querySelector("[action-add-account]").addEventListener("click", function (e) {
-		swal({
+	document.querySelector("[action-add-account]").addEventListener("click", () => addAccount());
+
+	const addAccount = () => {
+		const options = {
 			title: "Account name",
 			text: "Enter the name of your ProtonMail account",
 			type: "input",
 			confirmButtonText: "Add account",
-			
-		}, function (name) {
-			if (name !== false) {
-				createTab(name);
-			}
+		};
+		const onConfirmCallback = (name) => !!name ? createTab(name) : null;
+
+		swal(options, onConfirmCallback);
+	};
+	
+	const createTab = (name, active = false) => {
+		tabGroup.addTab({
+			title: name,
+			src: "https://mail.protonmail.com/login?",
+			visible: true,
+			closable: false,
+			active: true,
+			ready: (tab) => onTabReady(tab)
 		});
-	});
+	};
+
+	const onTabReady = (tab) => {
+		const domReadyEvent = () => {
+			prefillUsernameInLoginForm(tab.webview.getWebContents(), tab.title);
+			
+			tab.webview.removeEventListener('dom-ready', domReadyEvent);
+		};
+
+		tab.webview.addEventListener('dom-ready', domReadyEvent);
+	};
+	
+	const prefillUsernameInLoginForm = (webContents, username) => {
+		for (let character of username) {
+			webContents.sendInputEvent({
+				type: 'char',
+				keyCode: character
+			});
+		}
+	};
 };
