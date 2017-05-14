29bd65d66dd17e284dfd371a3efeb7ee4c68a130
Matthew C <imatcore@gmail.com>
2017-05-14 13:45:12 +0200

    New compact design
    Bugs to fix:
    - Email input takes title variable and autoinput takes only 1st letter
    - Css could be fancier, not my strong

--- src/sidebar.js
+++ src/sidebar.js
@@ -2,10 +2,10 @@ export class Sidebar {
 	constructor(page) {
 		const TabGroup = require('electron-tabs');
 		this.tabGroup = new TabGroup();
-		
+
 		this.addEventListenerForAddAccount();
 	}
-	
+
 	addEventListenerForAddAccount() {
 		document.querySelector("[action-add-account]").addEventListener("click", () => this.addAccount());
 	}
@@ -17,12 +17,13 @@ export class Sidebar {
 			text: "Enter the name of your ProtonMail account",
 			type: "input",
 			confirmButtonText: "Add account",
+			showCancelButton: true,
 		};
-		const onConfirmCallback = (name) => !!name ? this.createTab(name) : null;
-		
+		const onConfirmCallback = (name) => !!name ? this.createTab(name.substr(0,1)) : null;
+
 		swal(options, onConfirmCallback);
 	}
-	
+
 	createTab(name, active = false) {
 		this.tabGroup.addTab({
 			title: name,
@@ -33,17 +34,17 @@ export class Sidebar {
 			ready: (tab) => this.onTabReady(tab)
 		});
 	}
-	
+
 	onTabReady(tab) {
 		const domReadyEvent = () => {
 			this.prefillUsernameInLoginForm(tab.webview.getWebContents(), tab.title);
-			
+
 			tab.webview.removeEventListener("dom-ready", domReadyEvent);
 		};
-		
+
 		tab.webview.addEventListener("dom-ready", domReadyEvent);
 	}
-	
+
 	prefillUsernameInLoginForm(webContents, username) {
 		for (let character of username) {
 			webContents.sendInputEvent({
