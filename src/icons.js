84c230a173f2360bf712e7eac2e52899d41172d0
kontrollanten <alexander@width.se>
2017-08-14 00:04:58 +0200

    Moved icon paths to separate file

---
+++ src/icons.js
@@ -0,0 +1,7 @@
+import path from 'path';
+const basePath = path.join(__dirname, 'static');
+
+export const ICONS = {
+  TRAY: basePath.concat('/IconTray.png'),
+  TRAY_UNREAD: basePath.concat('/IconTrayUnread.png'),
+};
