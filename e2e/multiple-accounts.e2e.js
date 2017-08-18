e94618b3229c53a8e09f3012b2b1f662e4d656fe
kontrollanten <alexander@width.se>
2017-08-18 03:30:20 +0200

    Added test for prefill of username

--- e2e/multiple-accounts.e2e.js
+++ e2e/multiple-accounts.e2e.js
@@ -10,19 +10,25 @@ describe('multiple accounts', function () {
 
   it('creates a new tab when account is added', function () {
     const accountName = 'awesomeMail';
-    
+
     return this.app.client.waitUntilWindowLoaded()
       .waitForVisible('button.add-account', 10000)
-      .pause(500)
       .click('button.add-account')
       .waitForVisible('.sweet-alert input[type=text]')
       .setValue('.sweet-alert input[type=text]', accountName)
-      .pause(500)
+      .pause(200)
       .click('button.confirm')
+      .pause(200)
       .getText('.etabs-tabs .etabs-tab-title')
       .then(text => {
         expect(typeof text).equal('string');
         expect(text.toLowerCase()).equal(accountName.slice(0, 1).toLowerCase());
+      })
+      .windowByIndex(1)
+      .waitForVisible('#pm_login #username')
+      .getValue('#pm_login #username')
+      .then(username => {
+        expect(username).equal(accountName);
       });
   });
 });
