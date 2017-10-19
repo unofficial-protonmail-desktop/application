c035e7e8e146137190453e1109beb614f9f32172
Alexander <alexander@width.se>
2017-10-20 01:30:30 +0200

    autoupdate: add unit tests

---
+++ src/auto-updater.spec.js
@@ -0,0 +1,47 @@
+import { initiateAutoUpdater } from "./auto-updater";
+
+const { app, dialog }  = require('electron');
+const { autoUpdater } = require('electron-updater');
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const expect = chai.expect;
+
+chai.use(sinonChai);
+
+describe('AutoUpdater', () => {
+  it('should check for updates on app ready', done => {
+    sinon.stub(autoUpdater, 'checkForUpdates');
+
+    app.on('ready', () => {
+      setTimeout(() => {
+
+        expect(autoUpdater.checkForUpdates).to.have.been.called;
+        done();
+      });
+    });
+
+    initiateAutoUpdater();
+    app.emit('ready');
+  });
+
+  it('should show a dialog when an error occurs', () => {
+    sinon.stub(dialog, 'showMessageBox');
+
+    initiateAutoUpdater();
+    autoUpdater.emit('error');
+
+    expect(dialog.showMessageBox).to.have.been.called;
+    dialog.showMessageBox.restore();
+  });
+
+  it('should show adialog when a new update has downloaded', () => {
+    sinon.stub(dialog, 'showMessageBox');
+
+    initiateAutoUpdater();
+    autoUpdater.emit('update-downloaded', {});
+
+    expect(dialog.showMessageBox).to.have.been.called;
+    dialog.showMessageBox.restore();
+  });
+});
