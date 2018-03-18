4e5a994eed5fb1a8acd5190a157dc1e00192043c
kontrollanten <alexander@width.se>
2018-03-18 13:20:29 +0100

    chore: fix linting errors

--- config/stylelint.config.js
+++ config/stylelint.config.js
@@ -2,4 +2,16 @@ module.exports = {
   extends: [
     "stylelint-config-standard",
   ],
+
+
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "extend",
+        ],
+      },
+    ],
+  }
 };
