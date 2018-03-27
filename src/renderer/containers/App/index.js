54b14be383c7cb130ebadb7ce9c8b8014ad82296
kontrollanten <alexander@width.se>
2018-03-27 21:52:54 +0200

    chore(devtools): add redux and react devtools

--- src/renderer/containers/App/index.js
+++ src/renderer/containers/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import webviewHandler from '../../lib/webview-handler';
@@ -13,7 +13,11 @@ import MailBox from '../MailBox';
 
 import WebviewsMw from '../../middlewares/Webviews';
 
-const store = createStore(AppState, applyMiddleware(WebviewsMw));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  AppState, 
+  composeEnhancers(applyMiddleware(WebviewsMw)),
+);
 
 export default class App extends React.Component {
   setWebviewContainerElem(elem) {
