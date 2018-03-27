c42d5c24c133ed7df7602177b125fa64c6c96382
kontrollanten <alexander@width.se>
2018-03-27 22:54:13 +0200

    fix(add-account): redirect on submit
    When a user submits the form, he gets redirected to the MailBox

--- src/renderer/containers/AddAccount.js
+++ src/renderer/containers/AddAccount.js
@@ -1,15 +1,19 @@
 import { connect } from 'react-redux';
 
+import history from '../history';
 import { ADD_ACCOUNT } from './App/types';
 import AddAccount from '../components/AddAccount';
 
 const mapStateToProps = () => ({});
 const mapDispatchToProps = dispatch => ({
-  onSubmit: account => 
+  onSubmit: account => {
     dispatch({
       type: ADD_ACCOUNT,
       account,
-    }),
+    });
+
+    history.push('mailbox/'.concat(account.username));
+  },
 });
 
 export default connect(
