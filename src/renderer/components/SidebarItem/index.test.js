21d033810a8216512c746c9be5dfd3dc8b571baa
kontrollanten <alexander@width.se>
2018-05-22 19:28:25 +0200

    refactor(sidebar): split to two comps

---
+++ src/renderer/components/SidebarItem/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import SidebarItem from './';
+
+describe('components/SidebarItem', () => {
+  it('should display a notifications badge when unreadEmails is given', () => {
+    const unreadEmails = 12;
+    const context = shallow(<SidebarItem
+      href=""
+      isActive={false}
+      unreadEmails={unreadEmails}
+      username=""
+    />);
+
+    expect(context.find('span.NotificationBadge').length).to.equal(1);
+    expect(context.find('span.NotificationBadge').text()).to.equal(unreadEmails.toString());
+  });
+
+});
