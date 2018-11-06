23f325913cd46525d0b1b3ab30394b9643047a29
kontrollanten <alexander@width.se>
2018-11-07 00:28:50 +0100

    refactor(app): split UI to components

--- src/renderer/containers/App/index.js
+++ src/renderer/containers/App/index.js
@@ -1,61 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Router, Route } from 'react-router-dom';
-
-import webviewHandler from '../../lib/webview-handler';
-import history from '../../history';
 import { updateIconBadge } from './actions';
+import App from '../../components/App';
 
-import AddAccount from '../AddAccount';
-import Wrapper from '../../components/Wrapper';
-import Sidebar from '../Sidebar';
-import Settings from '../Settings';
-import MailBox from '../MailBox';
-
-class App extends React.Component {
-  static propTypes = {
-    onMount: PropTypes.func.isRequired,
-  };
-
-  state = {
-    webviewReady: false,
-  };
-
-  componentDidMount() {
-    this.props.onMount();
-  }
-
-  setWebviewContainerElem(elem) {
-    if (webviewHandler.container) return;
-    webviewHandler.attachTo(elem);
-
-    this.setState({
-      webviewReady: true,
-    });
-  }
-
-  render() {
-    return (
-      <Router history={history}>
-        <Wrapper>
-          <Sidebar />
-
-          <Route path="/settings" component={Settings} />
-          <Route path="/add-account" component={AddAccount} />
-          <Route
-            path="/mailbox/:username"
-            render={props => this.state.webviewReady && <MailBox
-              webviewHandler={webviewHandler}
-              {...props.match.params}
-            />}
-          />
-          <div ref={this.setWebviewContainerElem.bind(this)} />
-        </Wrapper>
-      </Router>
-    );
-  }
-}
 
 const mapDispatchToProps = dispatch => ({
   onMount: () => dispatch(updateIconBadge()),
