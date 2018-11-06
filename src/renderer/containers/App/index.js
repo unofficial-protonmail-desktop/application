6c696e6b384158eeefd971fcbff556ba0248faa5
kontrollanten <alexander@width.se>
2018-11-07 00:59:51 +0100

    fix(app): redirect user to accurate view upon load
    if the user doesnt have any account, redirect to /add-account, otherwise redirect to first account

--- src/renderer/containers/App/index.js
+++ src/renderer/containers/App/index.js
@@ -2,12 +2,15 @@ import { connect } from 'react-redux';
 import { updateIconBadge } from './actions';
 import App from '../../components/App';
 
+const mapStateToProps = ({ accounts }) => ({
+  firstAccount: Object.values(accounts)[0]
+});
 
 const mapDispatchToProps = dispatch => ({
   onMount: () => dispatch(updateIconBadge()),
 });
 
 export default connect(
-  () => ({}),
+  mapStateToProps,
   mapDispatchToProps,
 )(App);
