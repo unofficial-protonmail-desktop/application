34cb8c4a19b0b4f156f32f6f2828a0e77b1a0d40
kontrollanten <alexander@width.se>
2018-11-10 23:55:16 +0100

    fix(auto-updater): show notification when auto update not available

--- src/main/auto-updater.spec.js
+++ src/main/auto-updater.spec.js
@@ -1,4 +1,4 @@
-const { app, dialog }  = require('electron');
+const { app, dialog, shell }  = require('electron');
 const { autoUpdater } = require('electron-updater');
 const chai = require('chai');
 const sinon = require('sinon');
@@ -10,39 +10,70 @@ chai.use(sinonChai);
 const { initiateAutoUpdater } = require('./auto-updater');
 
 describe('AutoUpdater', () => {
-  it('should check for updates on app ready', () => {
-    sinon.stub(autoUpdater, 'checkForUpdates');
-    sinon.spy(app, 'on');
+  const sandbox = sinon.createSandbox();
 
-    initiateAutoUpdater();
+  beforeEach(() => {
+    sandbox.stub(autoUpdater, 'checkForUpdatesAndNotify');
+    sandbox.stub(autoUpdater, 'checkForUpdates');
+    sandbox.spy(autoUpdater, 'on');
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should check for updates on app ready', () => {
+    sandbox.spy(app, 'on');
+    autoUpdater.checkForUpdatesAndNotify.returns(Promise.resolve());
+
+    initiateAutoUpdater({});
 
     const onAppReadyCb = app.on.getCalls().filter(call => call.calledWith('ready')).pop().args[1];
     onAppReadyCb();
 
-    expect(autoUpdater.checkForUpdates).to.have.been.called;
-    autoUpdater.checkForUpdates.restore();
+    expect(autoUpdater.checkForUpdatesAndNotify).to.have.been.called;
   });
 
   it('should show a dialog when an error occurs', () => {
-    sinon.stub(dialog, 'showMessageBox');
-    sinon.spy(autoUpdater, 'on');
+    sandbox.stub(dialog, 'showMessageBox');
 
-    initiateAutoUpdater();
+    initiateAutoUpdater({});
 
     const onAutoUpdaterErrorCb = autoUpdater.on.getCalls().filter(call => call.calledWith('error')).pop().args[1];
-    onAutoUpdaterErrorCb();
+    onAutoUpdaterErrorCb({});
 
+    expect(autoUpdater.on).to.have.been.called;
     expect(dialog.showMessageBox).to.have.been.called;
-    dialog.showMessageBox.restore();
   });
 
-  it('should show adialog when a new update has downloaded', () => {
-    sinon.stub(dialog, 'showMessageBox');
+  describe('auto update not supported', () => {
+    let onReady;
 
-    initiateAutoUpdater();
-    autoUpdater.emit('update-downloaded', { releaseNotes: '' });
+    beforeEach(() => {
+      sandbox.stub(dialog, 'showMessageBox');
+      autoUpdater.checkForUpdatesAndNotify.returns(Promise.resolve(null));
+      onReady = sandbox.stub(app, 'on').withArgs('ready');
+    });
 
-    expect(dialog.showMessageBox).to.have.been.called;
-    dialog.showMessageBox.restore();
+    it('should show a dialog when update-available occurs', async () => {
+      initiateAutoUpdater();
+      await onReady.lastCall.args[1]();
+      autoUpdater.emit('update-available', { releaseNotes: '' });
+
+      expect(dialog.showMessageBox).to.have.been.called;
+    });
+
+    it('should open external application to download when the user clicks on the first button', async () => {
+      sandbox.stub(shell, 'openExternal');
+
+      initiateAutoUpdater();
+      await onReady.lastCall.args[1]();
+      autoUpdater.emit('update-available', { releaseNotes: '' });
+
+      const onDialogClick = dialog.showMessageBox.lastCall.args[2];
+      onDialogClick(0);
+
+      expect(shell.openExternal).to.have.been.calledWith(sinon.match(/https/));
+    });
   });
 });
