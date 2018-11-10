545d8e4178afdb331a3d61c26eec267a4506abf9
kontrollanten <alexander@width.se>
2018-11-10 23:55:16 +0100

    fix(auto-updater): show notification when auto update not available

--- src/main/auto-updater.js
+++ src/main/auto-updater.js
@@ -1,20 +1,42 @@
-const { app, dialog, nativeImage }  = require('electron');
+const { app, dialog, nativeImage, shell }  = require('electron');
 const { autoUpdater } = require('electron-updater');
-
+const packageJson = require('../../package.json');
 const { ICONS } = require('./icons');
 
 export const initiateAutoUpdater = () => {
-  autoUpdater.autoDownload = true;
+  app.on('ready', async () => {
+    const response = await autoUpdater.checkForUpdatesAndNotify();
 
-  app.on('ready', () => {
-    autoUpdater.checkForUpdates();
+    if (response === null) {
+      autoUpdater.on('update-available', updateInfo => {
+        dialog.showMessageBox(null, {
+          type: 'info',
+          title: 'Update of Protonmail Desktop available!',
+          message: `Protonmail Desktop ${updateInfo.version} is ready to be installed!`,
+          detail: updateInfo.releaseNotes.replace(/<[^>]+>/g, ''),
+          defaultId: 0,
+          cancelId: 1,
+          icon: nativeImage.createFromPath(ICONS.TRAY),
+          buttons: [
+            'Download update',
+            'Postpone this update',
+          ],
+        }, r => {
+          if (r === 1) return;
+
+          shell.openExternal(packageJson.homepage.concat('/releases'));
+        });
+      });
+
+      autoUpdater.checkForUpdates();
+    }
   });
 
   autoUpdater.on('error', error => {
     dialog.showMessageBox(null, {
       type: 'error',
       title: 'Something went wrong',
-      message: 'Ops! ProtonMail Desktops auto updater failed. Check ' +
+      message: 'ProtonMail Desktops auto updater failed. Check ' +
       'https://github.com/protonmail-desktop/application for the latest updates and ' +
       'please create an issue.',
       details: JSON.stringify(error),
@@ -22,25 +44,5 @@ export const initiateAutoUpdater = () => {
         'Okey'
       ]
     }, () => true);
-  });
-
-  autoUpdater.on('update-downloaded', updateInfo => {
-    dialog.showMessageBox(null, {
-      type: 'info',
-      title: 'Update of Protonmail Desktop available!',
-      message: `Protonmail Desktop ${updateInfo.version} is ready to be installed!`,
-      detail: updateInfo.releaseNotes.replace(/<[^>]+>/g, ''),
-      buttons: [
-        'Install update and restart app',
-        'Postpone this update',
-      ],
-      defaultId: 0,
-      cancelId: 1,
-      icon: nativeImage.createFromPath(ICONS.TRAY),
-    }, response => {
-      if (response === 1) return;
-
-      autoUpdater.quitAndInstall();
-    });
   });
 };
