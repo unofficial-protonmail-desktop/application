894189cba19503066a4ab2aadc227eb5962c6c27
kontrollanten <alexander@width.se>
2018-11-19 14:35:15 +0100

    fix(notifications): show accurate value when count is 0

--- src/renderer/components/SidebarItem/index.test.js
+++ src/renderer/components/SidebarItem/index.test.js
@@ -5,19 +5,28 @@ import { expect } from 'chai';
 import { SidebarItem } from './';
 
 describe('components/SidebarItem', () => {
+  const defaultProps = {
+    href: '',
+    isActive: false,
+    onOpenMenu: () => null,
+    onRemoveAccount: () => null,
+    username: '',
+  };
+
   it('should display a notifications badge when unreadEmails is given', () => {
     const unreadEmails = 12;
-    const context = shallow(<SidebarItem
-      href=""
-      isActive={false}
-      onOpenMenu={() => {}}
-      onRemoveAccount={() => {}}
+    const wrapper = shallow(<SidebarItem
+      {...defaultProps}
       unreadEmails={unreadEmails}
-      username=""
     />);
 
-    expect(context.find('span.NotificationBadge').length).to.equal(1);
-    expect(context.find('span.NotificationBadge').text()).to.equal(unreadEmails.toString());
+    expect(wrapper.find('span.NotificationBadge').length).to.equal(1);
+    expect(wrapper.find('span.NotificationBadge').text()).to.equal(unreadEmails.toString());
   });
 
+  it('should not display a notification badge when unreadEmails is 0', () => {
+    const wrapper = shallow(<SidebarItem {...defaultProps} unreadEmails={0} />);
+
+    expect(wrapper.find('span.NotificationBadge').length).to.equal(0);
+  });
 });
