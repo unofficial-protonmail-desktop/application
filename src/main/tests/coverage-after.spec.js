a0a178ce512a74e26d61ebe4df5cf3f21f8a7188
kontrollanten <alexander@width.se>
2019-03-09 10:50:14 +0100

    chore(electron-mocha): remove coverage support from renderer process
    Since we only use electron-mocha for main process, this code is
    obsolete.

--- src/main/tests/coverage-after.spec.js
+++ src/main/tests/coverage-after.spec.js
@@ -1,12 +1,6 @@
-import {remote} from 'electron';
 import glob from 'glob';
 import path from 'path';
-
-const resolveDependency = name => {
-  return remote ? remote.require(name) : require(name);
-};
-
-const fs = resolveDependency('fs-extra');
+import fs from 'fs-extra';
 
 const writeCoverageReport = coverage => {
   const outputFile = path.resolve(process.cwd(), 'coverage/coverage-main.json');
